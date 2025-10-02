539b6f2ea564fb6d50f455d03d91c75ed3faae40
yuzhou2 <yuzhou2@andrew.cmu.edu>
2025-10-02 02:42:28 +0000

    Unit test aligned

--- tests/runtime_python/cute/hopper/test_matmul_ws_hopper.py
+++ tests/runtime_python/cute/hopper/test_matmul_ws_hopper.py
@@ -10,18 +10,9 @@ reduction_size = 4096
 batch_size = 16
 output_size = 64
 
-x = torch.randn(
-    (reduction_size, batch_size), device="cuda", dtype=torch.bfloat16, generator=g
-)
+x = torch.randn((batch_size, reduction_size), device="cuda", dtype=torch.bfloat16, generator=g)
 weight = torch.randn((output_size, reduction_size), device="cuda", dtype=torch.bfloat16, generator=g)
-for i in range(reduction_size):
-    for j in range(batch_size):
-        x[i, j] = 0.2
-for i in range(output_size):
-    for j in range(reduction_size):
-        weight[i, j] = 0.1
 
-# x = torch.randn((batch_size, reduction_size), device="cuda", dtype=torch.bfloat16, generator=g)
 residual = torch.zeros(batch_size, output_size, device="cuda", dtype=torch.bfloat16)
 
 output = torch.empty(batch_size, output_size, device="cuda", dtype=torch.bfloat16)
@@ -29,7 +20,7 @@ output = torch.empty(batch_size, output_size, device="cuda", dtype=torch.bfloat1
 runtime_kernel_cute_hopper.linear(weight, x, residual, output)
 
 # torch_out = torch.matmul(weight, x)
-torch_out = torch.matmul(x.T, weight.T)
+torch_out = torch.matmul(x, weight.T)
 torch_out = torch_out + residual
 print(output.shape)
 print(torch_out.shape)
